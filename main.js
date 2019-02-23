cc602cb4f2316e7a3edaa6e5d03faee70a387c09
overpuntoplus <vidatraslapc.host@gmail.com>
2019-02-23 01:06:12 -0200

    update to 1.0.2

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const {app, BrowserWindow, Tray, Menu, nativeImage} = require('electron');
 const {autoUpdater} = require("electron-updater");
 const isDev = require("electron-is-dev");
 
+
 let ventana;
 let carga;
 
@@ -10,6 +11,54 @@ function ventanaPrincipal () {
 	if(!isDev){
 		autoUpdater.checkForUpdatesAndNotify();
 	}
+	
+	
+	
+	
+let win;
+
+function sendStatusToWindow(text) {
+  log.info(text);
+  win.webContents.send('message', text);
+}
+function createDefaultWindow() {
+  win = new BrowserWindow();
+  win.webContents.openDevTools();
+  win.on('closed', () => {
+    win = null;
+  });
+  win.loadURL(`file://${__dirname}/version.html#v${app.getVersion()}`);
+  return win;
+}
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+});
+	
+	
+createDefaultWindow();
+	
+	
+	
+	
+	
 	
 	const path = require('path');
 	const iconPath = path.join(__dirname, 'icon.png');
